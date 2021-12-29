1fd422a54b76d74c60666c91e92d21c81d7f2171
Anupam Basak <anupam00basak@gmail.com>
2021-12-29 22:07:16 +0530

    Initial commit

---
+++ app/src/main/java/com/sbdev/insta_youtube_video_downloader/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sbdev.insta_youtube_video_downloader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
